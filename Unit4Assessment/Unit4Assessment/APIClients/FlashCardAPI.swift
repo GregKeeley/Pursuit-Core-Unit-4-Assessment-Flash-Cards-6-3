194fa33f9b7129a16cf4f00d9f2ba18b266f7230
Gregory Keeley <gregorykeeley@pursuit.org>
2020-02-11 11:12:32 -0500

    - Built API client for Flash cards

--- Unit4Assessment/Unit4Assessment/APIClients/FlashCardAPI.swift
+++ Unit4Assessment/Unit4Assessment/APIClients/FlashCardAPI.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import NetworkHelper
+
+struct FlashCardAPI {
+    static func getFlashCards(completion: @escaping (Result<FlashCard,AppError>) -> ()) {
+        let endpoint = ""
+                guard let url = URL(string: endpoint) else {
+                    completion(.failure(.badURL(endpoint)))
+                    return
+                }
+                let request = URLRequest(url: url)
+                NetworkHelper.shared.performDataTask(with: request) { (result) in
+                    switch result {
+                    case .failure(let appError):
+                        completion(.failure(.networkClientError(appError)))
+                    case .success(let flashCardData):
+                        do {
+                            let flashCards = try JSONDecoder().decode(FlashCard.self, from: flashCardData)
+                            completion(.success(flashCards))
+                        } catch {
+                            completion(.failure(.decodingError(error)))
+                        }
+                    }
+                }
+            }
+        }
