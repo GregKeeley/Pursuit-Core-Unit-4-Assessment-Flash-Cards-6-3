673e850658eb3aaeaa59178ed84fa6204869e7e9
Gregory Keeley <gregorykeeley@pursuit.org>
2020-02-11 10:31:09 -0500

    - Created basic test/Model - Created additional supporting files

---
+++ Unit4Assessment/Unit4Assessment/Models/FlashCard.swift
@@ -0,0 +1,20 @@
+//
+//  FlashCard.swift
+//  Unit4Assessment
+//
+//  Created by Gregory Keeley on 2/11/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+
+struct FlashCard: Codable & Equatable {
+    let cardListType: String
+    let cards: Cards
+}
+struct Cards: Codable & Equatable {
+    let id: String
+    let cardTitle: String
+    let facts: String
+}
